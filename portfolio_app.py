17f929a900fc8c7144d43fb10d467d539d8c0f2d
Shurui-Liu <shurui.liu.710@gmail.com>
2025-04-06 15:31:24 -0700

    updated the links

--- portfolio_app.py
+++ portfolio_app.py
@@ -7,8 +7,8 @@ from PIL import Image
 st.set_page_config(page_title="My Portfolio", page_icon="💼", layout="wide")
 
 # ---- HEADER ----
-st.title("👋 Hi, I'm Your Name Here")
-st.subheader("Aspiring Software Engineer | Passionate about Full-Stack & AI")
+st.title("👋 Hi, I'm Shurui")
+st.subheader("Software Engineer | Passionate about Full-Stack & AI")
 st.write("Welcome to my interactive portfolio. Below are some of the projects I've worked on.")
 
 # ---- PROJECTS ----
@@ -16,23 +16,23 @@ st.markdown("## 🚀 Projects")
 
 project_data = [
     {
-        "title": "Trip Planner App",
-        "description": "Web-based travel planner using React, Flask, and Map APIs. Calculates optimized travel routes with user preferences.",
-        "tech": "Python, Flask, React, OpenStreetMap",
-        "link": "https://github.com/yourusername/trip-planner"
+        "title": "AI Mood Companion App",
+        "description": "an AI-empowered, multi-user SaaS web application tool for mental well-being. I developed the project's part that integrates LLM to offer real-time chats. Through prompt-engineering, I ensured this app help people identify their distorted thinking patterns and turn them into positive ones.",
+        "tech": "React, MongoDB, OpenAI API",
+        "live website": "https://moodcompanion.onrender.com/chat"
+    },
+    {
+    "title": "NYC Trip Planner",
+    "description": "A full-stack web application that helps users plan custom NYC trips by selecting starting points, destinations, and time constraints. The app uses optimization algorithms to generate the shortest multi-day itinerary that minimizes commuting time and maximizes visit coverage.",
+    "tech": "JavaScript, Express, Python, Google Maps API",
+    "link": "https://github.com/Shurui-Liu/NYC-Trip-Planner-Web"
     },
     {
         "title": "Grocery Store Inventory System",
         "description": "Internal system for managing inventory and staff, using SpringBoot and MySQL with JSP frontend.",
         "tech": "Java, SpringBoot, MySQL, JSP",
         "link": "https://github.com/yourusername/grocery-inventory-system"
-    },
-    {
-        "title": "Gradebook App",
-        "description": "Python-based tool to manage and visualize grades, with CSV import/export and statistical insights.",
-        "tech": "Python, Pandas, Streamlit",
-        "link": "https://github.com/yourusername/gradebook-app"
-    },
+    }
 ]
 
 for project in project_data:
